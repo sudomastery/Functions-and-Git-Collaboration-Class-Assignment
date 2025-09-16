950b7fd8e68f18e0328967cd36e0b77230ade118
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-09-16 10:35:21 +0300

    add demerits point calculation for speeds above 70

--- speed_check.py
+++ speed_check.py
@@ -4,4 +4,8 @@ def check_speed(speed):
     # if speed is less than 70 print ok
     if speed <70:
         print('ok')
-check_speed(70)
+    else:
+        # calculate demerits points for every 5 km/s above 70
+        points = (speed-70)
+        print (f'points:{points}')
+check_speed(95)
