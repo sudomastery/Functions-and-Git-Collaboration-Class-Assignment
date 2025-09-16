ca6cfea524149cc220ecc57d38471dbc3002fb0d
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-09-16 10:40:44 +0300

    add license suspension logic for demerit points above 12

--- speed_check.py
+++ speed_check.py
@@ -5,7 +5,10 @@ def check_speed(speed):
     if speed <70:
         print('ok')
     else:
-        # calculate demerits points for every 5 km/s above 70
+    # calculate demerits points for every 5 km/s above 70
         points = (speed-70)
         print (f'points:{points}')
-check_speed(95)
+    #  if demerit points are more than 12 function to print license suspended
+        if points>12:
+            print('License suspended')
+check_speed(83)
