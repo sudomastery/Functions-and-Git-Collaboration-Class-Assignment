ea220456fe474097a8014e5e9dc60d5b2b80f84c
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-09-16 10:22:02 +0300

    add speed check function for speed less than 70

--- speed_check.py
+++ speed_check.py
@@ -1,1 +1,7 @@
-# test code 
+# function check_speed that takes a parameter speed
+
+def check_speed(speed):
+    # if speed is less than 70 print ok
+    if speed <70:
+        print('ok')
+check_speed(70)
