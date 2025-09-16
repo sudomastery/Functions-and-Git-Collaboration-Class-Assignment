da16d629def8d3eefa4b05b6bbe77282ff10b15d
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-09-16 19:25:18 +0300

    paye calc

--- salary_calc.py
+++ salary_calc.py
@@ -17,3 +17,26 @@ print(f'NSSF Deduction: Ksh {nssf_deduction:.2f}')
 # SHIF deduction
 shif_deduction=gross_salary*0.0275
 print(f'SHIF Deduction: Ksh{shif_deduction:.2f}')
+
+# paye calculation
+
+taxable_income=gross_salary-nssf_deduction-shif_deduction
+
+# paye
+
+if taxable_income<=24000:
+    paye=taxable_income * 0.10
+elif taxable_income<=32333:
+    paye=(24000*0.10)+((taxable_income-24000))*0.25
+elif taxable_income<=500000:
+    paye=(24000*0.10)+((32333-24000)*0.25)+((taxable_income-32333)*0.30)
+elif taxable_income<=800000:
+    paye=(24000*0.10)+((32333-24000)*0.25)+((50000-32333)*0.30)+((taxable_income-50000)*0.35)
+else:
+    paye=(24000*0.10)+((32333-24000)*0.25)+((50000-32333)*0.30)+((80000-50000)*0.35)+((taxable_income-80000)*0.37)
+
+# personal relief calc
+personal_relief=2400
+paye=max(paye-personal_relief,0)
+
+print(f'PAYE: Ksh{paye:.2f}')
