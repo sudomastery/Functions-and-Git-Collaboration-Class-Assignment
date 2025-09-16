a543898c73673f1196a6f762fc5e9cb2333131f4
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-09-16 17:58:47 +0300

    Gross salary calc after user basic sal and benefits input

--- salary_calc.py
+++ salary_calc.py
@@ -1,1 +1,10 @@
-# testing code 
+# basic salary input by user
+basic_salary=float(input('Enter your basic salary (Ksh): '))
+# user enters their benefits
+benefits=float(input('Enter your benefits (Ksh): '))
+
+# calculate gross salary
+gross_salary=basic_salary+benefits
+
+# print the gross salary
+print(f'Gross salary: Ksh {gross_salary:.2f}')
